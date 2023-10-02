d908e6e036aea44683bce55f4cc5869a765caef1
asw825 <63994741+asw825@users.noreply.github.com>
2023-10-02 16:39:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-be
+ 코알누나랑 함께만드는 toapp backend
---
+++ controller/task.controller.js
@@ -0,0 +1,25 @@
+const Task = require("../model/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save();
+    res.status(200).json({ status: "ok", data: newTask });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+taskController.getTask = async (req, res) => {
+  try {
+    const taskList = await Task.find({}).select("-__v");
+    res.status(200).json({ status: "ok", data: taskList });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports = taskController;
