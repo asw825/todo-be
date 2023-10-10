edc169aa6e632d9f01063d178d3e590ae8f04bbd
asw825 <63994741+asw825@users.noreply.github.com>
2023-10-10 21:12:51 +0900

    feat: "cors 처리와 , aws 배포환경세팅"

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
-
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log(MONGODB_URI_PROD);
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://127.0.0.1:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI)
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("err!!", err));
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
