b40f9d3caab5ff52ef5889ca056f16590dffc1a0
asw825 <63994741+asw825@users.noreply.github.com>
2023-10-18 01:06:35 +0900

    feat : "implemented login and register page with jwt,bcrypt"

---
+++ model/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//toJSON 은 mongoose에서 제공하는 함수이다. 프론트에서는 JSON 으로 데이터타입이 바뀌어야하는데 Object에서 JSON 으로 바꿀때 해당 함수를 사용해서 프론트에가기전에 미리가로채 작업을 해줄수있다.
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
